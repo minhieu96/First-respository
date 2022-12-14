591e7c0ac8e7517d5d15f48b56916bc1c9fd3cd8
rezasaedi1 <118889998+rezasaedi1@users.noreply.github.com>
2022-12-14 13:18:24 +0330

    add general template folder
    add general template folder to website project

--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from django.http import HttpResponse ,JsonResponse
 
 def index_page(request):
-    return render(request,'web/index.html')
+    # return render(request,'web/index.html')
+    return render(request,'index.html')
 
 def account_page(request):
     return render(request,'my-account.html')
